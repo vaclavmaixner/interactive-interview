04d4c5d2a041639868592c32aede5a0e7fce0b7d
vaclavmaixner <vaclavmaixnerj@gmail.com>
2019-09-03 18:36:35 +0200

    Last save before deleting AJAX.

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function changeRightSide(heading) {
     document.getElementById("right-heading").innerHTML = heading;
 }
+
+$(document).ready(function(){
+
+    // jQuery methods go here...
+  
+  });
