bcacaa675039d75bc30e03f528788f401d99a99f
vaclavmaixner <vaclavmaixnerj@gmail.com>
2019-09-03 20:12:47 +0200

    Pred pridanim Reactu.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function changeRightSide(heading) {
     document.getElementById("right-heading").innerHTML = heading;
+    document.getElementById("right-content").innerHTML = '<object type="text/html" data="'+ heading.toLowerCase() +'.html" ></object>';
 }
 
 $(document).ready(function(){
